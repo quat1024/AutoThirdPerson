159fd45ad8d7aac70ea9d5d0ea6ab202f15e4647
quat1024 <quat1024@users.noreply.github.com>
2023-01-17 23:40:49 -0500

    Do people even play 1.17.1? anyway here it is.

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # Auto Third Person but it's for ~~forge~~ ~~fabric~~ fabric and forge ~~1.12~~ ~~1.16~~ ~~1.17~~ ~~1.18~~ ~~1.19~~ everything???
 
-Puts you in third person when you do certain things.
+Puts you in third person when you do certain things. This used to be a simple mod.
 
 LGPL 3.0 or later.
 
 ## Warning
 
-You need a *lot* of RAM to run `build`, which builds every project at the same time. `gradle.properties` allows Gradle 4 gigabytes of RAM, which will only grow as I add more projects. Try commenting out some subproject declarations in `settings.gradle` if you're having issues.
+You need a *lot* of RAM to run `build`, which builds every project. `gradle.properties` allows Gradle 4 gigabytes of RAM, which will only grow as I add more projects. Try commenting out some subproject declarations in `settings.gradle` if you're having issues. (As a side effect, if you're moving to a new computer, compiling this project is a great way to populate your Gradle cache.)
 
-Parallel building has been disabled in `gradle.properties`, because there seems to be some bug where if you invoke multiple Looms at the same time, they stomp on each other. Apologies in advance for the lost performance.
+Parallel building has been disabled in `gradle.properties`, because there seems to be some bug where if you invoke multiple Looms at the same time they stomp on each other, and I wouldn't be surprised if there was a ForgeGradle bug too. Apologies in advance for the lost performance.
 
-Loom will sometimes explode with some nonsense about being unable to parse the version numbers provided in the `fabricApi.module` calls. If this happens just try again. (this might be related to the race condition) 
+Loom will sometimes explode with some nonsense about being unable to parse the version numbers provided in the `fabricApi.module` calls. If this happens just try again. (this might be related to the race condition)
+
+If ForgeGradle explodes with something about "ProjectScopeServices has been closed." that's some forgegradle bug with the daemon. `./gradlew --stop` fixes it. Yes, even if the daemon is already disabled, intellij sync always uses the daemon.
 
 ## Subprojects guided tour
 
---
+++ Xplat-1.17.1/src/main/java/agency/highlysuspect/autothirdperson/SeventeenOneMinecraftInteraction.java
@@ -0,0 +1,158 @@
+package agency.highlysuspect.autothirdperson;
+
+import net.minecraft.client.CameraType;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.Registry;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.vehicle.Boat;
+import net.minecraft.world.entity.vehicle.Minecart;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.ref.WeakReference;
+
+public class SeventeenOneMinecraftInteraction implements MinecraftInteraction {
+	private final Minecraft client = Minecraft.getInstance();
+	private final MyLogger logger = new Log4jMyLogger(LogManager.getLogger(AutoThirdPerson.NAME));
+	
+	@Override
+	public MyLogger getLogger() {
+		return logger;
+	}
+	
+	@Override
+	public boolean hasElytra() {
+		return true;
+	}
+	
+	@Override
+	public boolean hasSwimmingAnimation() {
+		return true;
+	}
+	
+	@Override
+	public MyCameraType getCameraType() {
+		return wrapCameraType(client.options.getCameraType());
+	}
+	
+	@Override
+	public void setCameraType(MyCameraType type) {
+		client.options.setCameraType(unwrapCameraType(type));
+	}
+	
+	@Override
+	public boolean debugScreenUp() {
+		return client.options.renderDebug;
+	}
+	
+	@Override
+	public boolean safeToTick() {
+		return client.player != null && client.level != null && !client.isPaused();
+	}
+	
+	@Override
+	public boolean playerIsElytraFlying() {
+		assert client.player != null;
+		return client.player.isFallFlying();
+	}
+	
+	@Override
+	public boolean playerInSwimmingAnimation() {
+		assert client.player != null;
+		return client.player.isSwimming();
+	}
+	
+	@Override
+	public boolean playerIsUnderwater() {
+		assert client.player != null;
+		return client.player.isUnderWater();
+	}
+	
+	///
+	
+	public MyCameraType wrapCameraType(CameraType type) {
+		return switch(type) {
+			case FIRST_PERSON -> MyCameraType.FIRST_PERSON;
+			case THIRD_PERSON_BACK -> MyCameraType.THIRD_PERSON;
+			case THIRD_PERSON_FRONT -> MyCameraType.THIRD_PERSON_REVERSED;
+		};
+	}
+	
+	public CameraType unwrapCameraType(MyCameraType type) {
+		return switch(type) {
+			case FIRST_PERSON -> CameraType.FIRST_PERSON;
+			case THIRD_PERSON -> CameraType.THIRD_PERSON_BACK;
+			case THIRD_PERSON_REVERSED -> CameraType.THIRD_PERSON_FRONT;
+		};
+	}
+	
+	public Vehicle wrapVehicle(Entity ent) {
+		return new EntityVehicle(ent);
+	}
+	
+	private static class EntityVehicle implements Vehicle {
+		public EntityVehicle(Entity ent) {
+			this.ent = new WeakReference<>(ent);
+			this.id = ent == null ? "<nothing>" : Registry.ENTITY_TYPE.getKey(ent.getType()).toString();
+			
+			if(ent instanceof Minecart) this.type = VehicleClassification.MINECART;
+			else if(ent instanceof Boat) this.type = VehicleClassification.BOAT;
+			else if(ent instanceof Animal) this.type = VehicleClassification.ANIMAL;
+			else this.type = VehicleClassification.OTHER;
+		}
+		
+		private final WeakReference<Entity> ent;
+		private final String id;
+		private final VehicleClassification type;
+		
+		@Override
+		public boolean stillExists() {
+			Entity entity = ent.get();
+			return entity != null && entity.isAlive();
+		}
+		
+		@Override
+		public @NotNull String id() {
+			return id;
+		}
+		
+		@Override
+		public @NotNull VehicleClassification classification() {
+			return type;
+		}
+		
+		@Override
+		public boolean vehicleEquals(Vehicle other) {
+			if(!(other instanceof EntityVehicle)) return false;
+			Entity myEntity = ent.get();
+			Entity otherEntity = ((EntityVehicle) other).ent.get();
+			return myEntity != null && myEntity.isAlive() && myEntity == otherEntity;
+		}
+	}
+	
+	@SuppressWarnings("ClassCanBeRecord")
+	private static class Log4jMyLogger implements MyLogger {
+		public Log4jMyLogger(Logger logger) {
+			this.logger = logger;
+		}
+		
+		private final Logger logger;
+		
+		@Override
+		public void info(String msg, Object... args) {
+			logger.info(msg, args);
+		}
+		
+		@Override
+		public void warn(String msg, Object... args) {
+			logger.warn(msg, args);
+		}
+		
+		@Override
+		public void error(String msg, Throwable err) {
+			logger.error(msg, err);
+		}
+	}
+}
