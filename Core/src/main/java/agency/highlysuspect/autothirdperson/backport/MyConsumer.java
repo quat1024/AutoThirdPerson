7312ddb4588bedba8045266b35b3d69d7c87b3a4
quat1024 <quat1024@users.noreply.github.com>
2023-08-30 17:48:05 -0400

    Delete unused java8 backports

--- Core/src/main/java/agency/highlysuspect/autothirdperson/backport/MyConsumer.java
+++
@@ -1,9 +0,0 @@
-package agency.highlysuspect.autothirdperson.backport;
-
-/**
- * Exactly the same as Java 8's Consumer, but Java 6 compatible.
- * @param <T> The type of thing to consume.
- */
-public interface MyConsumer<T> {
-	void accept(T thing);
-}
